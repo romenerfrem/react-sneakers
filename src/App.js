ff4c7386aa1a6387baea1cc836929ef5aa5e93b0
romen.efrem <romen.efrem@yandex.ru>
2022-03-23 17:18:16 +0300

    Добавил хранениет товара из корзины на сервере, удаление и добавление

--- src/App.js
+++ src/App.js
@@ -1,42 +1,74 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Header from './components/Header';
 import Card from './components/Card/Card';
 import Dawer from './components/Dawer';
 
-const arr = [
-  {name: 'Мужские кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.jpg' },  
-  {name: 'Мужские кроссовки Nike Air Max 270', price: 15600, imageUrl: '/img/sneakers/2.jpg' },
-  {name: 'Мужские кроссовки Nike Mid Suede', price: 14999, imageUrl: '/img/sneakers/3.jpg' },
-  {name: 'Мужские кроссовки Nike Air Max 280', price: 15800, imageUrl: '/img/sneakers/4.jpg' }
-]
+
 
 
 function App() {
 
+  const [items, setItems] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
   const [cartOpened, setCartOpened] = useState(false);
 
+
+  useEffect(()=>{
+      axios.get('https://620e05f3585fbc3359d41fe5.mockapi.io/items').then((res) =>(
+        setItems(res.data)
+      ));
+      axios.get('https://620e05f3585fbc3359d41fe5.mockapi.io/cart').then((res) =>(
+        setCartItems(res.data)
+      ));
+  }, []);
+
+
+
+
+
+  const onAddToCart = (obj) =>{
+    axios.post('https://620e05f3585fbc3359d41fe5.mockapi.io/cart', obj);
+    setCartItems(prev => [...prev, obj])
+  }
+  const onRemoveItem = (id) =>{
+    axios.delete(`https://620e05f3585fbc3359d41fe5.mockapi.io/cart/${id}`);
+    setCartItems(prev => prev.filter(item => item.id !== id));
+  }
+
+
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value);
+  }
+  
+  
   return (
     <div className="wrapper clear">
-      {cartOpened ?  <Dawer onClose={() => setCartOpened(false)}/> : null}
+      {cartOpened ?  <Dawer items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem}/> : null}
     
     <Header onClickCart={() => setCartOpened(true)} />
 
       <div className="content p-40 ">
         <div className="d-flex align-center mb-40 justify-between">
-          <h1>Все кроссовки</h1>
+          <h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все кроссовки' }</h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search" />
-            <input placeholder="Поиск..." />
+            {searchValue && <img onClick={()=>setSearchValue('')} className='clear cu-p' src="/img/btn-remove.svg" alt="Close" />}
+            <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..." />
           </div>
         </div>
-        <div className="d-flex">
-          {arr.map((obj)=>(
+        <div className="d-flex flex-wrap">
+          {items
+          //.filter((item) => item.title.toLowerCase().includes(searchValue.toLowerCase()))
+          .map((item, index)=>(
             <Card
-              title = {obj.name}
-              price = {obj.price}
-              imageUrl = {obj.imageUrl}
-              onFavorite = {() => console.log('Дабавили взакладки')}
-              onPlus = {() => console.log('Нажали плюс')}
+              key = {index}
+              title = {item.name}
+              price = {item.price}
+              imageUrl = {item.imageUrl}
+              onFavorite = {() => console.log('Дабавили в закладки')}
+              onPlus = {(obj) => onAddToCart(obj)}
             
             /> 
            ))}
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import styles from './Card.module.scss';
 
-const Card = (props) => {
+const Card = ({onFavorite, imageUrl, title, price, onPlus}) => {
   const [isAdded, setIsAdded] = useState(false);
   const onClickPlus = () => {
+    onPlus({title, imageUrl, price});
     setIsAdded(!isAdded);
   }
 
@@ -13,15 +14,15 @@ const Card = (props) => {
 
   return (
         <div className={styles.card}>
-            <div className={styles.favorite} onClick={props.onFavorite}>
+            <div className={styles.favorite} onClick={onFavorite}>
               <img src="/img/unliked.svg" alt="Unliked" />
             </div>
-            <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
-            <h5>{props.title}</h5>
+            <img width={133} height={112} src={imageUrl} alt="Sneakers" />
+            <h5>{title}</h5>
             <div className="d-flex justify-between align-center">
               <div className="d-flex flex-column">
                 <span>Цена:</span>
-                <b>{props.price} руб.</b>
+                <b>{price} руб.</b>
               </div>
               <img className={styles.plus} onClick={onClickPlus} src={isAdded ? "/img/btn-checked.svg" :"/img/btn-plus.svg"} alt="plus" />
             </div>
